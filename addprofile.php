8bca9d459287328f6345e0d9f630a6991e66be76
Raeka Attari <raekaattari@wustl.edu>
2019-09-25 03:59:57 -0500

    3:59

---
+++ addprofile.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+require 'database.php';
+
+if (!isset($_SESSION['username'])){
+    // Username already taken
+    echo "sorry! you need to be logged in to post!";
+    exit; 
+}
+
+if(isset($_POST['submit'])){
+    $title = $_POST['title'];
+    $my_story = $_POST['mystory'];
+    $links = $_POST['link'];
+}
+
+$stmt = $mysqli->prepare("insert into stories (title, story, username, link) values (?, ?, ?, ?) ");
+
+if(!$stmt){
+	printf("Query Prep Failed: %s\n", $mysqli->error);
+	exit;
+}
+
+$stmt->bind_param('ssss', $title, $my_story, $_SESSION['username'], $links);
+
+
+
+$stmt->execute();
+
+$stmt->close();
+
+header("Location: newsite.html");
+exit;
+
+?>
